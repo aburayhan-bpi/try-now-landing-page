a1944dbae7304e39625b276122024db611f92491
Abu Rayhan <aburayhan.bpi@gmail.com>
2025-04-13 18:30:40 +0600

    flash sale products slider added

---
+++ src/components/common/ProductCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { IoStar } from "react-icons/io5";
+
+const ProductCard = (product = {}) => {
+  console.log(product);
+  return (
+    <div>
+      <div className="relative border w-full h-full px-2 py-4 group rounded-lg transition duration-300">
+        <div className="bg-gray-200 flex items-center justify-center h-64 p-4 rounded-tl-full rounded-tr-full transition duration-300 group-hover:bg-sky-100">
+          <img
+            className="w-40 h-40 object-contain transition-transform duration-300"
+            src={product?.product?.image}
+            alt={product?.title}
+          />
+        </div>
+        <div className="absolute top-10 left-0 bg-black text-white px-5 py-2 rounded-lg">
+          {product?.product?.discount} OFF
+        </div>
+        <div className="mt-3">
+          <h2 className="font-medium text-lg">{product?.product?.title}</h2>
+          <p className="text-green-500 font-medium capitalize">
+            {product?.product?.availability}
+          </p>
+          <div className="flex gap-1 items-center mt-2">
+            <span className="text-yellow-500 flex items-center justify-center gap-1 mr-1">
+              {[...Array(5)].map((_, i) => (
+                <IoStar
+                  key={i}
+                  className={
+                    i < product?.product?.rating
+                      ? "text-sky-500"
+                      : "text-gray-300"
+                  }
+                />
+              ))}
+            </span>
+            <span className="font-medium">{product?.rating}</span>
+          </div>
+          <div className="flex gap-2 mt-2 text-sm">
+            <span className="line-through text-gray-600 opacity-50">
+              {product?.product?.regularPrice}
+            </span>
+            <span className="font-medium text-sky-500">
+              {product?.product?.discountPrice}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import EyeglassSlider from "../../components/EyeglassSlider";
 import SectionHeading from "../../components/common/SectionHeading";
+import ProductCard from "../../components/common/ProductCard";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 const Home = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [transitionEnabled, setTransitionEnabled] = useState(true);
+  const slidesToShow = 4;
   const categoryGlassCase = [
     {
       _id: 1,
@@ -49,6 +54,98 @@ const Home = () => {
       alt: "Young Fashion's glass",
     },
   ];
+  const products = [
+    {
+      _id: 1,
+      title: "Gucci Eyeglasses",
+      image: "https://i.ibb.co/Gvkjmd0g/eyeglass3.png",
+      availability: "in stock",
+      rating: 3,
+      regularPrice: "$5.45",
+      discountPrice: "$3.53",
+      discount: "14%",
+    },
+    {
+      _id: 2,
+      title: "Ray-Ban Classic Wayfarer",
+      image: "https://i.ibb.co/60k4RHFR/eyeglass.png",
+      availability: "in stock",
+      rating: 5,
+      regularPrice: "$120.00",
+      discountPrice: "$99.00",
+      discount: "18%",
+    },
+    {
+      _id: 3,
+      title: "Oakley Men’s Metal Frame",
+      image: "https://i.ibb.co/Gvkjmd0g/eyeglass3.png",
+      availability: "out of stock",
+      rating: 4,
+      regularPrice: "$150.00",
+      discountPrice: "$129.99",
+      discount: "13%",
+    },
+    {
+      _id: 4,
+      title: "Prada Rectangular Eyeglasses",
+      image: "https://i.ibb.co/Gvkjmd0g/eyeglass3.png",
+      availability: "in stock",
+      rating: 4,
+      regularPrice: "$220.00",
+      discountPrice: "$189.99",
+      discount: "14%",
+    },
+    {
+      _id: 5,
+      title: "Versace Men’s Designer Glasses",
+      image: "https://i.ibb.co/60k4RHFR/eyeglass.png",
+      availability: "in stock",
+      rating: 5,
+      regularPrice: "$270.00",
+      discountPrice: "$215.50",
+      discount: "20%",
+    },
+    {
+      _id: 6,
+      title: "Burberry Square Optical Glasses",
+      image: "https://i.ibb.co/60k4RHFR/eyeglass.png",
+      availability: "limited stock",
+      rating: 4,
+      regularPrice: "$180.00",
+      discountPrice: "$159.99",
+      discount: "11%",
+    },
+    {
+      _id: 7,
+      title: "Tom Ford FT5401 Eyeglasses",
+      image: "https://i.ibb.co/60k4RHFR/eyeglass3.png",
+      availability: "in stock",
+      rating: 5,
+      regularPrice: "$350.00",
+      discountPrice: "$299.99",
+      discount: "14%",
+    },
+    {
+      _id: 8,
+      title: "Persol PO3007V Eyeglasses",
+      image: "https://i.ibb.co/60k4RHFR/eyeglass.png",
+      availability: "in stock",
+      rating: 4,
+      regularPrice: "$230.00",
+      discountPrice: "$199.00",
+      discount: "13%",
+    },
+  ];
+
+  const totalSlides = Math.ceil(products.length / slidesToShow);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % totalSlides);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + totalSlides) % totalSlides);
+  };
 
   return (
     <div className="max-w-screen-xl mx-auto px-4 xl:px-0">
@@ -57,7 +154,7 @@ const Home = () => {
       </div>
 
       {/* Eyeglasses Category Section */}
-      <div className="mb-40">
+      <div className="mb-14">
         <SectionHeading title={"Eyeglasses Categories"} />
 
         <div className=" flex flex-col lg:flex-row gap-8 items-stretch">
@@ -127,6 +224,79 @@ const Home = () => {
               </div>
             ))}
           </div>
+        </div>
+      </div>
+
+      {/* Flash Sale Section */}
+      <div className="mb-40">
+        <SectionHeading title={"Flash Sale"} />
+        {/* Timer */}
+        <div className="md:flex items-center justify-between max-w-2xl rounded-full border px-6 py-4">
+          <h2 className="text-lg text-center mb-4 md:text-3xl md:text-left md:mb-0 font-bold">
+            Deal of The Day
+          </h2>
+          <div className="md:flex items-center justify-center gap-3">
+            <span className="font-normal flex justify-center items-center mb-2 md:block md:mb-0">
+              End in:{" "}
+            </span>
+            <div className="flex items-center justify-center gap-3">
+              <div className="px-3 pt-5 pb-3 bg-sky-600 text-white rounded-lg">
+                00D
+              </div>
+              <div className="px-3 pt-5 pb-3 bg-sky-600 text-white rounded-lg">
+                00H
+              </div>
+              <div className="px-3 pt-5 pb-3 bg-sky-600 text-white rounded-lg">
+                00M
+              </div>
+              <div className="px-3 pt-5 pb-3 bg-sky-600 text-white rounded-lg">
+                00S
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Product Slider */}
+        <div className="mt-10 relative overflow-hidden">
+          <div
+            className="flex transition-transform duration-500 ease-in-out"
+            style={{
+              transform: `translateX(-${currentSlide * 100}%)`,
+              transition: transitionEnabled
+                ? "transform 500ms ease-in-out"
+                : "none",
+            }}
+          >
+            {products.map((product) => (
+              <div
+                key={product._id}
+                className="w-full flex-shrink-0 px-3"
+                style={{ width: `${100 / slidesToShow}%` }}
+              >
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+
+          {/* Navigation Arrows */}
+          {totalSlides > 1 && (
+            <>
+              <button
+                onClick={prevSlide}
+                className="absolute left-0 top-1/2 -translate-y-1/2 p-3 rounded-full z-10 transition-all duration-300 hover:scale-150"
+                aria-label="Previous slide"
+              >
+                <SlArrowLeft />
+              </button>
+              <button
+                onClick={nextSlide}
+                className="absolute right-0 top-1/2 -translate-y-1/2  p-3 rounded-full z-10 transition-all duration-300 hover:scale-150"
+                aria-label="Next slide"
+              >
+                <SlArrowRight />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
